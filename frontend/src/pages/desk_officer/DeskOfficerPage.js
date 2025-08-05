afce1e149f97d46f8cb9f4b703c7dcf13d64e1c7
Rhainer <126873901+Rhainer13@users.noreply.github.com>
2025-08-05 15:31:33 +0800

    Minor Changes

--- frontend/src/pages/desk_officer/DeskOfficerPage.js
+++ frontend/src/pages/desk_officer/DeskOfficerPage.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Sidebar from "./SideBar";
-import Navbar from "./NavBar";
+import Navbar from "./navBar";
+import Sidebar from "./sideBar";
 
 export default function DeskOfficerPage() {
   return (
--- frontend/src/pages/desk_officer/RegisterVictim.js
+++ frontend/src/pages/desk_officer/RegisterVictim.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Sidebar from "./SideBar";
-import Navbar from "./NavBar";
+import Sidebar from "./sideBar";
+import Navbar from "./navBar";
 import AdministrativeInfo from "./AdministrativeInfo";
 
 function isMinor(birthDate) {
