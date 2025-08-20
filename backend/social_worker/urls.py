96243b4c24a6d4d0563aba6dd6b36e4856796511
Harley <harleygit@gmail.com>
2025-08-21 01:28:19 +0800

    Working facial recognition in searching victim

--- backend/social_worker/urls.py
+++ backend/social_worker/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path("victims/", victim_list.as_view(), name="victim-list"),
     path("victims/<int:vic_id>/", victim_detail.as_view(), name="victim-detail"),
+    path("victims/search_face/", search_victim_facial.as_view(), name="victim-face-search"),
 ]
