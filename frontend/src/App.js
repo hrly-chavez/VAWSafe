1f18ecc9bdb83f8277e75bc9742e42dc28466000
Rhainer <126873901+Rhainer13@users.noreply.github.com>
2025-08-21 15:54:22 +0800

    Testing Multi Part Forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,11 @@ import RegisterUser from "./pages/RegisterUser";
 
 // desk officer
 import DeskOfficerPage from "./pages/desk_officer/DeskOfficer/DeskOfficerPage";
+import VictimFacial from "./pages/desk_officer/RegisterVictim/VictimFacial";
 import RegisterVictim from "./pages/desk_officer/RegisterVictim/RegisterVictim";
 import Start from "./pages/desk_officer/Session/Start";
-import VictimFacial from "./pages/desk_officer/RegisterVictim/VictimFacial";
+// for testing
+import Test from "./pages/desk_officer/Testing/Test.js";
 
 //DSWD
 import DSWDDashboard from "./pages/dswd/DSWDDashboard";
@@ -28,8 +30,6 @@ import Victims from "./pages/social_worker/Victims/Victims";
 import VictimDetailPage from "./pages/social_worker/Victims/VictimDetailPage";
 import SearchVictimFacial from "./pages/social_worker/Victims/SearchVictimFacial.js";
 
-
-
 export default function App() {
   return (
     <Router>
@@ -47,6 +47,8 @@ export default function App() {
         />
         <Route path="/desk_officer/victim_facial" element={<VictimFacial />} />
         <Route path="/desk_officer/start" element={<Start />} />
+        {/* for testing */}
+        <Route path="/test" element={<Test></Test>} />
 
         {/* DSWD Page */}
         {/* <Route path="/dswd" element={<DSWDPage />} /> */}
@@ -69,7 +71,10 @@ export default function App() {
           <Route path="services" element={<Services />} />
           <Route path="victims" element={<Victims />} />
           <Route path="victims/:vic_id" element={<VictimDetailPage />} />
-          <Route path="/social_worker/victims/search-facial" element={<SearchVictimFacial />}/>
+          <Route
+            path="/social_worker/victims/search-facial"
+            element={<SearchVictimFacial />}
+          />
         </Route>
       </Routes>
     </Router>
---
+++ frontend/src/pages/desk_officer/Testing/Page1.js
@@ -0,0 +1,25 @@
+export default function Page1({ formData, setFormData, next, cancel }) {
+  return (
+    <div>
+      <h2>Page 1</h2>
+      <input
+        type="text"
+        placeholder="First Name"
+        value={formData.firstName || ""}
+        onChange={(e) =>
+          setFormData({ ...formData, firstName: e.target.value })
+        }
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={formData.lastName || ""}
+        onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
+      />
+      <br />
+      <button onClick={cancel}>Cancel</button>
+      <button onClick={next}>Continue</button>
+    </div>
+  );
+}
