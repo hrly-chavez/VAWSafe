66945e3f9fb5633ce5c4ee664c5be25375185fd4
Rhainer <126873901+Rhainer13@users.noreply.github.com>
2025-08-22 12:34:39 +0800

    button rearrangement

--- frontend/src/pages/desk_officer/Session/Session.js
+++ frontend/src/pages/desk_officer/Session/Session.js
@@ -54,6 +54,7 @@ export default function Session() {
           <Form3
             // formData={formData}
             // setFormData={setFormData}
+            back={back}
             cancel={cancel}
           />
         );
