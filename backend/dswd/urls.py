19d24082a8403ec857cdb3b0cfa35899b1c280b4
Rhainer <126873901+Rhainer13@users.noreply.github.com>
2025-08-25 18:53:38 +0800

    Revert "updated view victim and integrated search victim for dswd module"
    This reverts commit ad3ce2966fd234999e5038b5587a9f5b458a1702.

--- backend/dswd/urls.py
+++ backend/dswd/urls.py
@@ -5,5 +5,4 @@ from .views import *
 urlpatterns = [
     path("victims/", ViewVictim.as_view(), name="view_victim"),
     path("victims/<int:vic_id>/", ViewDetail.as_view(), name="view_detail"),
-    path("victims/search-victim/", search_victim_facial.as_view(), name="search-face"),
 ]
