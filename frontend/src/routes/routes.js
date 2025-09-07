602d6d4c32c40b1a0b35d6116cd136f10a33e4ba
Rhainer <126873901+Rhainer13@users.noreply.github.com>
2025-09-07 12:38:44 +0800

    Minor Code Rearrangements
    Minor UI Changes in texts

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -3,6 +3,7 @@ export const ROUTES = {
   MANUAL_LOGIN: "/login/manual",
   REGISTER: "/register",
 
+  // desk officer pages
   DESK_OFFICER: "/desk_officer",
   DESK_OFFICER_REGISTER_VICTIM: "/desk_officer/register_victim",
   DESK_OFFICER_VICTIM_FACIAL: "/desk_officer/victim_facial",
@@ -10,18 +11,10 @@ export const ROUTES = {
 
   DESK_OFFICER_VICTIMS: "/desk_officer/victims",
   DESK_OFFICER_VICTIM_DETAIL: "/desk_officer/victims/:vic_id",
-  DESK_OFFICER_VICTIM_SEARCH: "/desk_officer/victims/search",
+  DESK_OFFICER_VICTIM_SEARCH: "/desk_officer/victims/search-facial",
   DO_TEST: "/desk_officer/test",
 
-  DSWD: "/dswd",
-  DSWD_VAWC_VICTIMS: "/dswd/victims",
-  DSWD_VICTIM_DETAIL: "/dswd/victims/:vic_id",
-  DSWD_SEARCH_VICTIM: "/dswd/victims/search",
-  DSWD_SOCIAL_WORKERS: "/dswd/social-workers",
-  DSWD_SOCIAL_WORKERS_DETAILS: "/dswd/social-workers/:of_id",
-  DSWD_VAWDESK_OFFICER: "/dswd/vawdesk-officer",
-  DSWD_SERVICES: "/dswd/services",
-
+  // social worker pages
   SOCIAL_WORKER: "/social_worker",
   SOCIAL_WORKER_DASHBOARD: "dashboard",
   SOCIAL_WORKER_CASE_RECORDS: "case-records",
@@ -30,4 +23,14 @@ export const ROUTES = {
   SOCIAL_WORKER_VICTIMS: "victims",
   SOCIAL_WORKER_VICTIM_DETAIL: "victims/:vic_id",
   SOCIAL_WORKER_SEARCH_FACIAL: "victims/search-facial",
+
+  // dswd pages
+  DSWD: "/dswd",
+  DSWD_VAWC_VICTIMS: "/dswd/victims",
+  DSWD_VICTIM_DETAIL: "/dswd/victims/:vic_id",
+  DSWD_SEARCH_VICTIM: "/dswd/victims/search",
+  DSWD_SOCIAL_WORKERS: "/dswd/social-workers",
+  DSWD_SOCIAL_WORKERS_DETAILS: "/dswd/social-workers/:of_id",
+  DSWD_VAWDESK_OFFICER: "/dswd/vawdesk-officer",
+  DSWD_SERVICES: "/dswd/services",
 };
