f710f72f7b80d56624f4716abc8a2087dd3f0d75
Jin <jin@gmail.com>
2025-12-03 14:30:40 +0800

    address fix sa victimdetailpage

--- frontend/src/pages/social_worker/Victims/VictimDetailPage.js
+++ frontend/src/pages/social_worker/Victims/VictimDetailPage.js
@@ -401,7 +401,7 @@ export default function VictimDetailPage() {
                   />
                   <Info
                     label="Current Address"
-                    value={get(victim, ["vic_current_address"])}
+                    value={get(victim, ["full_address"])}
                     icon={<HomeIcon className="h-4 w-4 text-gray-500" />}
                   />
                   <Info
